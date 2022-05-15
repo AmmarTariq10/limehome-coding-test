d5a7ae59a57b7395a6c94c1d4e6df03adbc3e8b8
Ammar Tariq <ammar.tariq@salsoft.net>
2022-05-15 20:14:26 +0500

    listing completed

--- src/Screens/ApartmentListScreen/index.js
+++ src/Screens/ApartmentListScreen/index.js
@@ -4,14 +4,22 @@ import Constants from '../../Constants'
 import ApartmentListView from './ApartmentListView.screen'
 const ApartmentListScreen = (props) => {
     const [selectedLocation, setSelectedLocation] = React.useState(0)
-    React.useEffect(()=>{
-        getPropertyList(32)
-    },[])
+    const [locations, setLocations] = React.useState([])
+    const getList = async () => {
+        try {
+            const res = await getPropertyList(32)
+            setLocations(res)
+        } catch (error) {
+        }
+    }
+    React.useEffect(() => {
+        getList()
+    }, [])
     return (
         <ApartmentListView
             onSelectedLocationChange={setSelectedLocation}
             selected={selectedLocation}
-            locations={Constants.locations}
+            locations={locations}
             navigation={props.navigation}
         />
     )
